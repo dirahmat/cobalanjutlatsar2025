c23037056d7509a17ff9c7124b1f4e2fb2ba9983
dirahmat <adila_rachmatika009@yahoo.co.id>
2025-12-05 09:38:18 +0700

    Update format_angka.py

--- utils/format_angka.py
+++ utils/format_angka.py
@@ -1,10 +1,16 @@
 import re
 
-def format_with_dots(num):
-    return f"{num:,}".replace(",", ".")
-
 def sanitize_number(s):
+    """Extract digits and convert to integer."""
     if not s:
         return 0
     digits = re.sub(r"[^\d]", "", str(s))
     return int(digits) if digits else 0
+
+
+def format_with_dots(num):
+    """Format integer with dot separators."""
+    try:
+        return f"{int(num):,}".replace(",", ".")
+    except:
+        return "-"
