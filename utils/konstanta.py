c99526a5536cdc1c371204eb00f2860f4c1eacda
dirahmat <adila_rachmatika009@yahoo.co.id>
2025-12-05 09:27:58 +0700

    Create konstanta.py

---
+++ utils/konstanta.py
@@ -0,0 +1,28 @@
+MIN_UPAH = {
+    2011: 1290000,
+    2012: 1529150,
+    2013: 2200000,
+    2014: 2441000,
+    2015: 2700000,
+    2016: 3100000,
+    2017: 3355750,
+    2018: 3648035,
+    2019: 3940973,
+    2020: 4267349,
+    2021: 4416187,
+    2022: 4573845,
+    2023: 4901798,
+    2024: 5067381,
+    2025: 5396761,
+}
+
+BULAN = [
+    "Januari", "Februari", "Maret", "April", "Mei", "Juni",
+    "Juli", "Agustus", "September", "Oktober", "November", "Desember"
+]
+
+BULAN_BY_TAHUN = {
+    2011: ["September", "Oktober", "November", "Desember"]
+}
+
+ROWS_PER_PAGE = 10
